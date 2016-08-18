1679ef37f3fff77d328d5bc7b5873899ec3d56b3
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-18 19:46:05 +0800

    Bug fix

--- app/controllers/SubscribeController.scala
+++ app/controllers/SubscribeController.scala
@@ -1,9 +1,40 @@
 package controllers
 
-import play.api.libs.json.JsValue
+import models._
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, AnyContent, Controller}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class SubscribeController extends Controller {
+
+  // a parser that transforms the request body into a subscription object.
+  val parseAsSubscription = parse.using { request =>
+    parse.json.map {
+      body =>
+        val emailId = (body \ "emailId").as[String]
+        val interval = (body \ "interval").as[String]
+        Subscription(emailId, interval)
+    }
+  }
+
+  implicit val subWrites = Json.writes[Subscription]
+
+  def getSubByParser = Action(parseAsSubscription) {
+    request =>
+      val subscription: Subscription = request.body
+      Ok(Json.toJson(subscription))
+  }
+
+  // tolerant parsers
+  // it ignores the content type header in the request and parses based on the type specified.
+  // Parse the body as Json without checking the Content-Type.
+  def subscribe22 = Action(parse.tolerantJson) {
+    request =>
+      val reqData: JsValue = request.body
+      val emailId = (reqData \ "email").as[String]
+      val interval = (reqData \ "interval").as[String]
+      Ok(s"added $emailId to subscriber's list and will send updates every $interval")
+  }
 
   def subscribeOld = Action {
     request =>
